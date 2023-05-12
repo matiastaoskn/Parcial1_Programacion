f27839ab33b143e0e4100ed61e635c67340e8c2a
Matias Skenen <93952537+matiastaoskn@users.noreply.github.com>
2023-05-12 09:27:25 -0300

    Update extra.py

--- extra.py
+++ extra.py
@@ -3,7 +3,7 @@ for personaje in lista:
         lista_habilidades = []
         razas = personaje["Raza"][0]
         if filtro == razas:
-            print(f"Nombre: {personaje['Nombre']}")
+            nombre = {personaje['Nombre']}")
             poder_Pelea = int((personaje["PoderdePelea"]) * 0.5) + int(personaje["PoderdePelea"])
             poderAtaque = int((personaje["PoderdeDefensa"]) * 0.7) + int(personaje["PoderdeDefensa"])
         for habilidad in personaje["Habilidades"]:
@@ -12,3 +12,6 @@ for personaje in lista:
             with open("nuevo.csv", "w", newline="", encoding="utf-8") as file:
                 writer = csv.writer(file)
                 writer.writerow(lista_habilidades)
+                writer.writerow(nombre)
+                writer.writerow(poder_Pelea)
+                writer.writerow(poderAtaque)
